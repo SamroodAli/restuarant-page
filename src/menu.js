0c4d0f3a0986e9e8b380ed8e66c8af068c3a377d
samroodAli <devsamrood@gmail.com>
2021-07-09 20:43:49 +0530

    Style:Style menu for restuarant page

--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,16 @@
-import { section, ul, li, div, p } from "./tags.js";
+import { section, ul, li, div, p, h1 } from "./tags.js";
 
 const item = (food, cost) => li(div([p(food), p(cost)]), "card");
 
 const items = [
   item("Shawarma", "500"),
-  item("pizza", "900"),
-  item("dumpling", "100"),
+  item("Pizza", "900"),
+  item("Fumpling", "100"),
+  item("Falafel", "200"),
+  item("Manthi", "100"),
+  item("Shawarma", "100"),
 ];
 
-const menu = () => section(ul(items), "menu-section");
+const menu = () => section([h1("Today's menu"), ul(items)], "menu-section");
 
 export default menu;
