0d5c0f7accf7c20d63212ad74077ac315ebb98d7
samroodAli <devsamrood@gmail.com>
2021-07-09 15:16:10 +0530

    Dom:Create heading elements in elements js

---
+++ src/tags.js
@@ -0,0 +1,8 @@
+import ele from "./element";
+
+export const h1 = (...args) => ele("h1", ...args);
+export const h2 = (...args) => ele("h2", ...args);
+export const h3 = (...args) => ele("h3", ...args);
+export const h4 = (...args) => ele("h4", ...args);
+export const h5 = (...args) => ele("h5", ...args);
+export const h6 = (...args) => ele("h6", ...args);
