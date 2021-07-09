3f2e58a0a5320e6e3a6e0b756d33f2260e3967f3
samroodAli <devsamrood@gmail.com>
2021-07-09 21:11:19 +0530

    Fix:Add more contact info and remove linter offenses

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,15 @@
 import { section, p } from './tags.js';
 
 const contact = () => section(
-  [p('Universe'), p('Milky Way'), p('Earth'), p('India')],
+  [
+    p('Universe'),
+    p('Milky Way'),
+    p('Earth'),
+    p('India'),
+    p('Kerala'),
+    p('Malappuram district'),
+    p('Phone: +999999888'),
+  ],
   'contact-section',
 );
 
