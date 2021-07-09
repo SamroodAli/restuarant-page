a8baf92404cfbd96aedc9037375c294a1c2d709d
samroodAli <devsamrood@gmail.com>
2021-07-09 18:48:34 +0530

    bufferFeat^:Add image to the home page

--- src/home.js
+++ src/home.js
@@ -1,5 +1,7 @@
-import { section } from "./tags.js";
+import { section, img } from "./tags.js";
+import image from "./restuarant.jpg";
 
-const home = () => section("Home");
+const home = () =>
+  section([img("", "", {}, { src: image, alt: "Ratchets Restuarant image" })]);
 
 export default home;
