c08b8495a4ca67d6056ea7317c9143f815f38da8
samroodAli <devsamrood@gmail.com>
2021-07-09 19:28:50 +0530

    Home:Create menu items for the menu page

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import home from "./home.js";
 import contact from "./contact.js";
 import menu from "./menu.js";
 
-const main = () => home();
+const main = () => menu();
 
 export default main;
--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,13 @@
 import { section, ul, li, div, p } from "./tags.js";
 
-const item = (food, cost) => li(div(p(food), p(cost)), "card");
+const item = (food, cost) => li(div([p(food), p(cost)]), "card");
 
-const items = [item("chicken", 500), item("pizza", 900), item("dumpling", 100)];
+const items = [
+  item("chicken", "500"),
+  item("pizza", "900"),
+  item("dumpling", "100"),
+];
 
-const menu = () => section(ul(items));
+const menu = () => section(ul(items), "menu-section");
 
 export default menu;
