bf1fba9aeedad72fefe0305bc8c63fab74103ea6
samroodAli <devsamrood@gmail.com>
2021-07-09 14:27:39 +0530

    Config:Loaders for css in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
