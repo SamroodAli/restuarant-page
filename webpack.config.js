5b260b11cee34e03543f2c139ad19f252fd58e69
samroodAli <devsamrood@gmail.com>
2021-07-11 17:15:40 +0530

    Fix:Fix script in package.json for dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
-  mode: "production",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
