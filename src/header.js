661b85438c18aa4e4d8ab9144e9312c14ccc0b92
samroodAli <devsamrood@gmail.com>
2021-07-11 16:38:10 +0530

    Refactor:Refactor links to seperate navLinks page

--- src/header.js
+++ src/header.js
@@ -1,42 +1,23 @@
-import {
-  header, nav, a, button, span, div,
-} from './tags.js';
+import { header, nav, a, button, span, div } from "./tags.js";
+import { brand, navLinks } from "./navLink";
 
 const hamburgerButton = button(
-  span('', 'navbar-toggler-icon'),
-  'navbar-toggler',
+  span("", "navbar-toggler-icon"),
+  "navbar-toggler",
   {},
-  { type: 'button', 'data-toggle': 'collapse', 'data-target': 'navbar' },
+  { type: "button", "data-toggle": "collapse", "data-target": "navbar" }
 );
 
-const brand = a(
-  'Ratchets',
-  'navbar-brand',
-  { margin: '1rem 0.5rem' },
-  { href: '#' },
-);
-
-const navLink = (link) => a(
-  link,
-  'nav-item nav-link',
-  { margin: '0 2rem', color: 'black' },
-  { href: '#' },
-);
-
-export const homeLink = navLink('Home');
-export const menuLink = navLink('Menu');
-export const contactLink = navLink('Contact');
-
-const navLinks = [homeLink, menuLink, contactLink];
 const navLinksContainer = div(
-  div(navLinks, 'navbar-nav'),
-  'collapse navbar-collapse',
+  div(navLinks, "navbar-nav"),
+  "collapse navbar-collapse",
   {},
-  { id: 'navbar' },
+  { id: "navbar" }
 );
 
 const navBarItems = [brand, navLinksContainer, hamburgerButton];
 
-const pageHeader = () => header(nav(navBarItems, 'navbar navbar-expand-lg navbar-light bg-light'));
+const pageHeader = () =>
+  header(nav(navBarItems, "navbar navbar-expand-lg navbar-light bg-light"));
 
 export default pageHeader;
--- src/main.js
+++ src/main.js
@@ -1,26 +1,27 @@
-import home from './home.js';
-import contact from './contact.js';
-import menu from './menu.js';
-import { homeLink, menuLink, contactLink } from './header.js';
-import { section } from './tags.js';
+import home from "./home.js";
+import contact from "./contact.js";
+import menu from "./menu.js";
+import { homeLink, menuLink, contactLink } from "./navLink";
 
-const main = () => section(home(), '', {}, { id: 'main-section' });
+import { section } from "./tags.js";
 
-homeLink.addEventListener('click', () => {
-  const mainContent = document.getElementById('main-section');
-  mainContent.innerHTML = '';
+const main = () => section(home(), "", {}, { id: "main-section" });
+
+homeLink.addEventListener("click", () => {
+  const mainContent = document.getElementById("main-section");
+  mainContent.innerHTML = "";
   mainContent.appendChild(home());
 });
 
-menuLink.addEventListener('click', () => {
-  const mainContent = document.getElementById('main-section');
-  mainContent.innerHTML = '';
+menuLink.addEventListener("click", () => {
+  const mainContent = document.getElementById("main-section");
+  mainContent.innerHTML = "";
   mainContent.appendChild(menu());
 });
 
-contactLink.addEventListener('click', () => {
-  const mainContent = document.getElementById('main-section');
-  mainContent.innerHTML = '';
+contactLink.addEventListener("click", () => {
+  const mainContent = document.getElementById("main-section");
+  mainContent.innerHTML = "";
   mainContent.appendChild(contact());
 });
 
